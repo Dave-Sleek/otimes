88a85ca6232ff152cbe7fb22fc657763050de9ed
Dave Enyi <enyidavid87@gmail.com>
2020-11-12 21:48:35 +0100

    Newc

--- location.js
+++ location.js
@@ -1,47 +1,40 @@
-let map, infoWindow;
+function showPosition() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showMap, showError);
+  } else {
+    alert("Sorry, your browser does not support HTML5 geolocation.");
+  }
+}
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
-  infoWindow = new google.maps.InfoWindow();
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+// Define callback function for successful attempt
+function showMap(position) {
+  // Get location data
+  lat = position.coords.latitude;
+  long = position.coords.longitude;
+  var latlong = new google.maps.LatLng(lat, long);
+
+  var myOptions = {
+    center: latlong,
+    zoom: 16,
+    mapTypeControl: true,
+    navigationControlOptions: {
+      style: google.maps.NavigationControlStyle.SMALL
     }
-  });
+  }
+
+  var map = new google.maps.Map(document.getElementById("embedMap"), myOptions);
+  var marker = new google.maps.Marker({ position: latlong, map: map, title: "You are here!" });
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation ?
-    "Error: The Geolocation service failed." :
-    "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
+// Define callback function for failed attempt
+function showError(error) {
+  if (error.code == 1) {
+    result.innerHTML = "You've decided not to share your position, but it's OK. We won't ask you again.";
+  } else if (error.code == 2) {
+    result.innerHTML = "The network is down or the positioning service can't be reached.";
+  } else if (error.code == 3) {
+    result.innerHTML = "The attempt timed out before it could get the location data.";
+  } else {
+    result.innerHTML = "Geolocation failed due to unknown error.";
+  }
 }
-// [END maps_map_geolocation]
